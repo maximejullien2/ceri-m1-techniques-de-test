3730c47559f026810a71422c3581c47ae3bb3a5b
maximejullien2 <maxime.jullien2@alumni.univ-avignon.fr>
2024-03-15 17:48:14 +0100

    TP4: Réalisation de la fonction createPokedex dans PokedexFactory . Modification de IPokedexFactoryTest pour remplacer les mock par les vrais implémentations.

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -2,10 +2,11 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+
 
 class IPokedexFactoryTest {
 
@@ -15,16 +16,18 @@ class IPokedexFactoryTest {
 
     @BeforeEach
     public void setUp(){
-        this.iPokedexFactory = Mockito.mock(IPokedexFactory.class);
-        this.iPokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
-        this.iPokemonFactory = Mockito.mock(IPokemonFactory.class);
-        when(this.iPokedexFactory.createPokedex(this.iPokemonMetadataProvider,this.iPokemonFactory)).thenReturn(Mockito.mock(IPokedex.class));
+        ArrayList<PokemonMetadata> arrayList = new ArrayList<>();
+        arrayList.add(new PokemonMetadata(0,"Bulbizarre",126,126,90));
+        arrayList.add(new PokemonMetadata(133,"Aquali",186,168,260));
+        this.iPokemonMetadataProvider = new PokemonMetadataProvider(arrayList);
+        this.iPokemonFactory = new PokemonFactory(iPokemonMetadataProvider);
+        this.iPokedexFactory = new PokedexFactory();
     }
 
 
     @Test
     void createPokedexTest() {
-        IPokedex iPokedex = this.iPokedexFactory.createPokedex(this.iPokemonMetadataProvider,this.iPokemonFactory);
+        IPokedex iPokedex = iPokedexFactory.createPokedex(iPokemonMetadataProvider,iPokemonFactory);
         assertInstanceOf(IPokedex.class,iPokedex);
     }
 }
